2094d159aa97af1fd67f43ffdf6fb63217a33185
Jianshuo Wang <jianshuo@hotmail.com>
2026-05-28 12:43:18 +0800

    Initial Commit

---
+++ vibelight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vibelight
+//
+//  Created by Jianshuo Wang on 2026/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vibelight/vibelightApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibelightApp.swift
+//  vibelight
+//
+//  Created by Jianshuo Wang on 2026/5/28.
+//
+
+import SwiftUI
+
+@main
+struct vibelightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
